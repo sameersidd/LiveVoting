6c830e4ac87f5f3b1fd94a21b6df4aebb8faa5e4
Sameer Siddiqui <ss.darkchaos98@gmail.com>
2019-07-07 20:55:05 +0300

    Changed DB config and Heroku config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	mongoURI: "",
+	mongoURI: "mongodb+srv://voice_test:sam27885@cluster0-vhtcy.mongodb.net/test?retryWrites=true&w=majority",
 	localURI: "mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb"
 };
